44b9bfe6eb9964f078de73f6a940d79f267db30b
rikeyone <xiehaocheng127@163.com>
2018-12-19 14:33:05 +0800

    [qemu] remove qemu-2.11.0 from our env
    desc: It will be extracted from tar ball by install script
    
    Change-Id: I933e55af74d8f8b04031968b41efd0e6f9da356a
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

--- qemu-2.11.0/roms/skiboot/external/pflash/progress.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROGRESS_H
-#define __PROGRESS_H
-
-void progress_init(unsigned long count);
-void progress_tick(unsigned long cur);
-void progress_end(void);
-
-#endif /* __PROGRESS_H */
--- qemu-2.11.0/roms/skiboot/opal-ci/fetch-debian-jessie-installer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/vmlinux -O debian-jessie-vmlinux
-wget http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/initrd.gz -O debian-jessie-initrd.gz
