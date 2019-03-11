c44218ca57134e7f67656c264741145e08717a32
rikeyone <xiehaocheng127@163.com>
2019-03-11 15:18:53 +0800

    script: add a start script for sdcard boot option
    Change-Id: I6e7811535c1bbd11707d78836e1eda51638580d9
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

---
+++ qemu_sd_start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+arch=$1
+
+ROOTDIR=`pwd`
+
+if [ "${arch}" = "" ];then
+	arch=arm64
+fi
+
+# generate cpio initramfs
+cd rootfs/${arch}/_install
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ../
+gzip initramfs.cpio
+
+cd ${ROOTDIR}
+
+if [ "${arch}" = "arm" ];then
+	qemu-system-arm -M vexpress-a9 \
+		-smp 4 \
+		-m 1024m \
+		-kernel linux-4.0/arch/arm/boot/zImage \
+		-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8" \
+		-dtb linux-4.0/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
+		-initrd rootfs/${arch}/initramfs.cpio.gz \
+		-nographic
+else
+	qemu-system-aarch64 -machine virt \
+		-cpu cortex-a57 \
+		-machine type=virt \
+		-nographic -m 2048 \
+		-smp 2 \
+		-kernel linux-4.0/arch/arm64/boot/Image \
+		-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8  trace_event=sched:*,timer:*,irq:* trace_buf_size=40M" \
+		-initrd rootfs/${arch}/initramfs.cpio.gz
+fi
